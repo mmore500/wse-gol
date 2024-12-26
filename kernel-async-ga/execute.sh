0c91c9cfb6ad85e5a963fc78af7d378bc3c8b4e3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-25 20:17:14 -0500

    Export singularity bindpath, too, for compat

--- kernel-async-ga/execute.sh
+++ kernel-async-ga/execute.sh
@@ -11,5 +11,6 @@ export APPTAINERENV_ASYNC_GA_NCOL="${ASYNC_GA_NCOL:-3}"
 export APPTAINERENV_ASYNC_GA_NROW="${ASYNC_GA_NROW:-3}"
 export APPTAINERENV_ASYNC_GA_NTRAIT="${ASYNC_GA_NTRAIT:-1}"
 export APPTAINER_BINDPATH="../cerebraslib:/cerebraslib"
+export SINGULARITY_BINDPATH="../cerebraslib:/cerebraslib"
 
 "${CS_PYTHON}" client.py ${ASYNC_GA_EXECUTE_FLAGS:-}
