9f445425ff6a5da852a8999f7fb429fe92ddd0af
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-25 21:00:29 -0500

    Dont trample $LOCAL

--- kernel-async-ga/execute.sh
+++ kernel-async-ga/execute.sh
@@ -9,16 +9,16 @@ echo "ASYNC_GA_EXECUTE_FLAGS ${ASYNC_GA_EXECUTE_FLAGS:-}"
 
 echo "setup LOCAL -------------------------------------------------------------"
 echo "LOCAL ${LOCAL:-}"
-export LOCAL="${LOCAL:-local}/bio240020p/$(uuidgen)"
-echo "LOCAL ${LOCAL}"
-mkdir -p "${LOCAL}"
-touch "${LOCAL}/.touch"
-cp -r "../cerebraslib" "${LOCAL}/cerebraslib"
+export MYLOCAL="${LOCAL:-local}/bio240020p/$(uuidgen)"
+echo "MYLOCAL ${MYLOCAL}"
+mkdir -p "${MYLOCAL}"
+touch "${MYLOCAL}/.touch"
+cp -r "../cerebraslib" "${MYLOCAL}/cerebraslib"
 
 export APPTAINERENV_ASYNC_GA_NCOL="${ASYNC_GA_NCOL:-3}"
 export APPTAINERENV_ASYNC_GA_NROW="${ASYNC_GA_NROW:-3}"
 export APPTAINERENV_ASYNC_GA_NTRAIT="${ASYNC_GA_NTRAIT:-1}"
-export APPTAINER_BINDPATH="${LOCAL}:/local:rw,../cerebraslib:/cerebraslib:rw"
-export SINGULARITY_BINDPATH="${LOCAL}:/local:rw,../cerebraslib:/cerebraslib:rw"
+export APPTAINER_BINDPATH="${MYLOCAL}:/local:rw,../cerebraslib:/cerebraslib:rw"
+export SINGULARITY_BINDPATH="${MYLOCAL}:/local:rw,../cerebraslib:/cerebraslib:rw"
 
 "${CS_PYTHON}" client.py ${ASYNC_GA_EXECUTE_FLAGS:-}
