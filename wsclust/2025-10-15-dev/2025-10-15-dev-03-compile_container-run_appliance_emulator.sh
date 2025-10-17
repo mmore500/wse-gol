23710eee304c1f9eee8c55b6ddc5399658bc9299
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-17 12:13:56 -0400

    Draft appliance emulator runscript

---
+++ wsclust/2025-10-15-dev/2025-10-15-dev-03-compile_container-run_appliance_emulator.sh
@@ -0,0 +1,130 @@
+#!//bin/bash
+
+set -euo pipefail
+
+echo "date $(date '+%Y-%m-%d %H:%M:%S')"
+echo "hostname $(hostname)"
+
+source "${HOME}/.env" || true
+
+cd "$(dirname "$0")"
+echo "PWD ${PWD}"
+cd "$(git rev-parse --show-toplevel)"
+echo "PWD ${PWD}"
+
+echo "git rev-parse HEAD $(git rev-parse HEAD)"
+
+###############################################################################
+echo "setup work environment -------------------------------------------------"
+###############################################################################
+
+WORKDIR="${TMPDIR:-/tmp}/$(basename "$0" .sh)"
+echo "WORKDIR ${WORKDIR}"
+if [ -d "${WORKDIR}" ]; then
+    echo "Clearing WORKDIR ${WORKDIR}"
+    rm -rf "${WORKDIR}"
+fi
+mkdir "${WORKDIR}"
+
+echo "creating venv"
+VENVDIR="${WORKDIR}/venv"
+echo "VENVDIR ${VENVDIR}"
+python3.8 -m venv "${VENVDIR}"
+source "${VENVDIR}/bin/activate"
+which python3
+
+echo "setting up venv"
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade uv
+python3 -m uv pip install -r "requirements_cs.txt"
+python3 -m uv pip install ./pylib_cs
+python3 -m uv pip freeze | tee "${WORKDIR}/pip-freeze.txt"
+python3 -m pylib_cs.cslc_wsclust_shim  # test install
+
+rsync -a "${PWD}/" "${WORKDIR}/source/"
+
+###############################################################################
+echo "setup compile ----------------------------------------------------------"
+###############################################################################
+export CSLC="${CSLC:-cslc}"
+echo "CSLC ${CSLC}"
+
+export ASYNC_GA_ARCH_FLAG="wse3"
+echo "ASYNC_GA_ARCH_FLAG ${ASYNC_GA_ARCH_FLAG}"
+
+WORKDIR_MNT="$(realpath path)"
+WORKDIR_MNT="${WORKDIR_MNT#/}"
+WORKDIR_MNT="${WORKDIR_MNT%%/*}"
+echo "WORKDIR_MNT ${WORKDIR_MNT}"
+export SINGULARITY_BIND="${SINGULARITY_BIND:+${SINGULARITY_BIND},}/${WORKDIR_MNT}:/${WORKDIR_MNT},/tmp:/tmp"
+echo "SINGULARITY_BIND ${SINGULARITY_BIND}"
+
+# export ASYNC_GA_FABRIC_DIMS="757,996"  # TODO these are PSC WSE2 fabric dims
+# echo "ASYNC_GA_FABRIC_DIMS ${ASYNC_GA_FABRIC_DIMS}"
+
+###############################################################################
+echo "do compile -------------------------------------------------------------"
+###############################################################################
+"${WORKDIR}/source/kernel-async-ga/compile.sh" | tee "${WORKDIR}/compile.log"
+
+###############################################################################
+echo "setup run -------------------------------------------------------------"
+###############################################################################
+mkdir -p "${WORKDIR}/run"
+cd "${WORKDIR}/run"
+echo "PWD ${PWD}"
+
+cp -r "${WORKDIR}/source/kernel-async-ga/cerebraslib" .
+cp -r "${WORKDIR}/source/kernel-async-ga/out" .
+cp -r "${WORKDIR}/source/kernel-async-ga/client.py" .
+cp -r "${WORKDIR}/source/kernel-async-ga/compconf.json" .
+
+"${WORKDIR}/source/kernel-async-ga/compile.sh" | tee "${WORKDIR}/compile.log"
+
+python3 - <<'EOF'
+import logging
+
+from cerebras.appliance import logger
+from cerebras.sdk.client import SdkLauncher
+
+
+logging.basicConfig(
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+)
+
+logging.info("entering SdkLauncher")
+with SdkLauncher(".", disable_version_check=True, simulator=True) as launcher:
+
+    logging.info("querying context info...")
+    response = launcher.run(
+        "env",
+        "pwd",
+        "ls",
+        """ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'""",
+    )
+    logging.info("... done!")
+    logging.info(response + "\n")
+
+    command = "cs_python client.py"
+    logging.info("command={command}")
+    logging.info(f"running command...")
+    response = launcher.run(command)
+    logging.info("... done!")
+    logging.info(response + "\n")
+
+    logging.info("querying context info...")
+    response = launcher.run(
+        "env",
+        "pwd",
+        "ls",
+        """ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'""",
+    )
+    logging.info("... done!")
+    logging.info(response + "\n")
+
+    logging.info("exiting SdkLauncher")
+
+logging.info("exited SdkLauncher")
+EOF
