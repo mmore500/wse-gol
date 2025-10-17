c6759eb9bce6920504bd1e57f284669b00eea8fc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-17 11:24:17 -0400

    Allow /local and /cerebraslib overrides for non-container workflow

--- kernel-async-ga/client.py
+++ kernel-async-ga/client.py
@@ -82,10 +82,13 @@ def hexify_binary_data(
 def hexify_genome_data(data: "np.ndarray", verbose: bool = False) -> typing.List[str]:
     return hexify_binary_data(data, nWav=nWav, verbose=False)
 
+log("- printenv")
+for k, v in sorted(os.environ.items()):
+    log(f"  - {k}={v}")
 
 log("- setting up temp dir")
 # need to add polars to Cerebras python
-temp_dir = f"/local/tmp/{uuid.uuid4()}"
+temp_dir = f"{os.getenv('ASYNC_GA_LOCAL_PATH')}/tmp/{uuid.uuid4()}"
 os.makedirs(temp_dir, exist_ok=True)
 atexit.register(shutil.rmtree, temp_dir, ignore_errors=True)
 log(f"  - {temp_dir=}")
@@ -137,7 +140,7 @@ def write_parquet_verbose(df: pl.DataFrame, file_name: str) -> None:
     log(f"saving df to {file_name=}")
     log(f"- {df.shape=}")
 
-    tmp_file = "/local/tmp.pqt"
+    tmp_file = f"{os.getenv('ASYNC_GA_LOCAL_PATH')}/tmp.pqt"
     df.write_parquet(tmp_file, compression="lz4")
     log("- write_parquet complete")
 
@@ -223,7 +226,7 @@ log(f" - {traitLoggerNumBits=} {traitLoggerDstreamAlgoName=}")
 
 genomeFlavor = compconf_data["ASYNC_GA_GENOME_FLAVOR:comptime_string"]
 log(f" - {genomeFlavor=}")
-genomePath = f"/cerebraslib/genome/{genomeFlavor}.csl"
+genomePath = f"{os.getenv('ASYNC_GA_CEREBRASLIB_PATH')}/genome/{genomeFlavor}.csl"
 log(f" - reading genome data from {genomePath}")
 genomeDataRaw = "".join(
     removeprefix(line, "//!").strip()
